cf2458ab821c09b28645e66a863492e2ef4ee3a7
NozomuTsuruta <tuno.0806@icloud.com>
2020-10-24 18:38:27 +0900

    server側のgraphqlを簡単に実装

---
+++ server/graphql/resolvers.ts
@@ -0,0 +1,8 @@
+/**
+ * @desc スキーマの実行結果を定義
+ */
+export const resolvers = {
+    Query: {
+        hello: () => 'hello world',
+    },
+};
---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import { ApolloServer } from 'apollo-server';
+import { resolvers } from './graphql/resolvers';
+import { typeDefs } from './graphql/typeDefs';
+
+const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers,
+});
+
+const start_server = async () => {
+    try {
+        const res = await server.listen(3000);
+        console.log(`Server ready at ${res.url}`);
+    } catch (res) {
+        console.log(res);
+    }
+};
+
+start_server();
