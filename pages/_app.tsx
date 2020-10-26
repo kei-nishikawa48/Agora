1c43a5bd8faad366b1aa6c7f06fcfed0bf172540
kei <xnkcelebrate48x@yahoo.co.jp>
2020-10-26 17:37:14 +0900

    [update]firebaseの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,24 +5,27 @@ import { ThemeProvider as MaterialUIThemeProvider } from '@material-ui/core/styl
 import { StylesProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { theme } from '../styles/theme';
+import { AuthProvider } from '../context/Auth';
 
 export default function App({ Component, pageProps }: AppProps) {
-    // Remove the server-side injected CSS.(https://material-ui.com/guides/server-rendering/)
-    useEffect(() => {
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles && jssStyles.parentNode) {
-            jssStyles.parentNode.removeChild(jssStyles);
-        }
-    }, []);
+  // Remove the server-side injected CSS.(https://material-ui.com/guides/server-rendering/)
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
 
-    return (
-        <StylesProvider injectFirst>
-            <MaterialUIThemeProvider theme={theme}>
-                <StyledComponentsThemeProvider theme={theme}>
-                    <CssBaseline />
-                    <Component {...pageProps} />
-                </StyledComponentsThemeProvider>
-            </MaterialUIThemeProvider>
-        </StylesProvider>
-    );
+  return (
+    <AuthProvider>
+      <StylesProvider injectFirst>
+        <MaterialUIThemeProvider theme={theme}>
+          <StyledComponentsThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </StyledComponentsThemeProvider>
+        </MaterialUIThemeProvider>
+      </StylesProvider>
+    </AuthProvider>
+  );
 }
