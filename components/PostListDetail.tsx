4886bac805fce6bc68081801ec518c5f5ae3fb75
kei <xnkcelebrate48x@yahoo.co.jp>
2020-10-29 21:02:25 +0900

    エラー除去

--- components/PostListDetail.tsx
+++ components/PostListDetail.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import Link from 'next/link';
-import { Post } from '../interfaces';
+
+type Post = {
+  title:String
+  id:Number
+  text:String
+}
 
 type PostListDetailProps = {
   item: Post;
