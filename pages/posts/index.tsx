97e8c8c6c9431c5a98a8a2b30098f2bd84cfa35b
kei <xnkcelebrate48x@yahoo.co.jp>
2020-11-01 11:42:13 +0900

    いらないファイルを削除

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -27,9 +27,6 @@ const WithStaticProps = ({ items }: Props) => (
 );
 
 export const getStaticProps: GetStaticProps = async () => {
-  // Example for including static props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
   const items: Post[] = samplePostData;
   return { props: { items } };
 };
