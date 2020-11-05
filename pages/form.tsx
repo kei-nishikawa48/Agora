1fbb862a03b0a3b0a49d1cc208f8b58c8113e35d
kei <xnkcelebrate48x@yahoo.co.jp>
2020-11-05 17:42:10 +0900

    formデータを所得

--- pages/form.tsx
+++ pages/form.tsx
@@ -6,11 +6,20 @@ import { MarkdownEditor } from '../components/MarkdownEditor';
 import Layout from '../components/Layout';
 import {useForm} from "react-hook-form"
 import ChipsArray from "../components/Tags"
+import {CREATE_ARTICLE} from "../client_hooks/articles"
+import {useMutation } from '@apollo/client';
 export default function Form() {
   const [value, set_value] = React.useState(``);
   const {register,handleSubmit,errors}=useForm()
-  const submit=()=>{
-
+  const [create_article, { data }] = useMutation(CREATE_ARTICLE);
+  const submit= async (detail:any)=>{
+    await create_article({variables:{
+    "user_id": "2",
+    "title": detail.title,
+    "tags": detail.title,
+    "text": value
+    }})
+    {data && console.log(data)}
   }
   return (
     <Layout>
@@ -21,12 +30,13 @@ export default function Form() {
       ref={register({required:true})}
       />
       {errors.title&&<p>タイトルを入力してください</p>}
-      <Button>タグ追加</Button>
+      {/* <Button>タグ追加</Button> */}
       <ChipsArray/>
-    </form>
     <Page>
       <MarkdownEditor  value={value} set_value={set_value} />
     </Page>
+      <Button type="submit">追加</Button>
+    </form>
     </Layout>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,13 @@
 import Link from "next/link"
 import Layout from '../components/Layout';
 import firebase from "../utils/Firebase"
-// import {get_users} from "../client_hooks/users"
-// import {get_articles} from "../client_hooks/articles"
-// import {get_comments} from "../client_hooks/comment"
 
 
 const IndexPage = () => {
-  // const comments = get_comments()
-  // const articles = get_articles() 
-  // const users=get_users()
+
   const logout=async()=>{
     await firebase.auth().signOut()
   }
-  // console.log(users)
-  // console.log(articles)
-  // console.log(comments)
   return (
  
   <Layout title="Home | Next.js + TypeScript Example">
