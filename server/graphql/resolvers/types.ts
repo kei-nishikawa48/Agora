93d9f1b994e1010d47cd17b99c58b761e9188954
NozomuTsuruta <tuno.0806@icloud.com>
2020-11-06 12:48:00 +0900

    signup実装

--- server/graphql/resolvers/types.ts
+++ server/graphql/resolvers/types.ts
@@ -2,4 +2,8 @@ import { Models } from '../../models';
 
 export type ResolverContext = {
   models: Models;
+  jwt: {
+    secret: string;
+    expiresIn: string;
+  };
 };
--- server/graphql/resolvers/user.ts
+++ server/graphql/resolvers/user.ts
@@ -1,6 +1,12 @@
 import { IResolvers } from 'apollo-server-express';
+import jwt from 'jsonwebtoken';
 import User from '../../models/user';
 import { ResolverContext } from './types';
+
+const createToken = async (user: User, secret: string, expiresIn: string) => {
+  const { id, email } = user;
+  return await jwt.sign({ id, email }, secret, { expiresIn });
+};
 
 export const user_resolvers: IResolvers<User, ResolverContext> = {
   Query: {
@@ -8,13 +14,15 @@ export const user_resolvers: IResolvers<User, ResolverContext> = {
     user: async (parent, { id }, { models }) => models.User.findByPk(id),
   },
   Mutation: {
-    create_user: async (parent, { name, email, uid }, { models }) =>
-      models.User.create({ name, email, uid }),
     delete_user: async (parent, { id }, { models }) =>
       models.User.destroy({ where: { id } }),
     update_user: async (parent, { id, name, email }, { models }) => {
       models.User.update({ name, email }, { where: { id } });
       return models.User.findByPk(id);
+    },
+    sign_up: async (parent, { name, email, password }, { models, jwt }) => {
+      const user = await models.User.create({ name, email, password });
+      return { token: createToken(user, jwt.secret, jwt.expiresIn) };
     },
   },
   User: {
