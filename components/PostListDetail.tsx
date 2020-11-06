8ecab59476d0518b6bb2a3a84bf27944861809de
kei <xnkcelebrate48x@yahoo.co.jp>
2020-11-06 17:27:11 +0900

    detailにformの内容を写した

--- components/PostListDetail.tsx
+++ components/PostListDetail.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import Link from 'next/link';
-
-const PostListDetail = ({ item:post }:any) => (
+import {Markdown} from "./Markdown"
+const PostListDetail = ({ item: post }: any) => (
   <div>
     <h1>Detail for {post.title}</h1>
     <p>ID: {post.id}</p>
-    <p>Description: {post.text}</p>
+    <Markdown value={post.text} />
     <Link href="/posts/edit" as={`/posts/${post.id}/edit`}>
       <button>編集</button>
     </Link>
