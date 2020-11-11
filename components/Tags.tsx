d8d54165459e29e02fe67d50d44b7914bafbc380
kei <xnkcelebrate48x@yahoo.co.jp>
2020-11-11 22:04:09 +0900

    デーを少し追加、選択形式に変更

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -1,65 +1,123 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  createStyles,
+  makeStyles,
+  useTheme,
+  Theme,
+} from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import Chip from '@material-ui/core/Chip';
-import Paper from '@material-ui/core/Paper';
-import TagFacesIcon from '@material-ui/icons/TagFaces';
 
-type Data={
-  key:Number,
-  label:String
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+      maxWidth: "100%",
+    },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: 2,
+    },
+    noLabel: {
+      marginTop: theme.spacing(3),
+    },
+  })
+);
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT *10.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const tags = [
+  'JavaScript',
+  'TypeScript',
+  'Ruby',
+  'Python',
+  'CSS',
+  'HTML',
+  'Java',
+  'PHP',
+  'C#',
+  'Swift',
+  'C++',
+  'COBOL',
+  'Go',
+  'Kotlin',
+  'Objective-C',
+  'Scala',
+  'VBA',
+  'SQL',
+  'MySQL',
+  'NoSQL',
+  'Oracle Database',
+  'PostgreSQL',
+  'SQLite',
+];
+
+function getStyles(name: string, personName: string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
 }
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexWrap: 'wrap',
-    listStyle: 'none',
-    padding: theme.spacing(0.5),
-    margin: 0,
-  },
-  chip: {
-    margin: theme.spacing(0.5),
-  },
-}));
-
-export default function ChipsArray() {
+export default function MultipleSelect() {
   const classes = useStyles();
-  const [chipData, setChipData] = React.useState([
-    { key: 0, label: 'Angular' },
-    { key: 1, label: 'jQuery' },
-    { key: 2, label: 'Polymer' },
-    { key: 3, label: 'React' },
-    { key: 4, label: 'Vue.js' },
-  ]);
+  const theme = useTheme();
+  const [tagName, set_tagName] = React.useState<string[]>([]);
 
-  const handleDelete = (chipToDelete:Data) => () => {
-    setChipData((chips) =>
-      chips.filter((chip) => chip.key !== chipToDelete.key)
-    );
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    set_tagName(event.target.value as string[]);
   };
 
   return (
-    <Paper component="ul" className={classes.root}>
-      {chipData.map((data) => {
-        let icon;
-
-        if (data.label === 'React') {
-          icon = <TagFacesIcon />;
-        }
-
-        return (
-          <li key={data.key}>
-            <Chip
-              icon={icon}
-              label={data.label}
-              onDelete={data.label === 'React' ? undefined : handleDelete(data)}
-              className={classes.chip}
-            />
-          </li>
-        );
-      })}
-    </Paper>
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-mutiple-chip-label">タグ</InputLabel>
+        <Select
+          labelId="demo-mutiple-chip-label"
+          id="demo-mutiple-chip"
+          multiple
+          value={tagName}
+          onChange={handleChange}
+          input={<Input id="select-multiple-chip" />}
+          renderValue={(selected) => (
+            <div className={classes.chips}>
+              {(selected as string[]).map((value) => (
+                <Chip key={value} label={value} className={classes.chip} />
+              ))}
+            </div>
+          )}
+          MenuProps={MenuProps}
+        >
+          {tags.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+              style={getStyles(name, tagName, theme)}
+            >
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>   
+    </div>
   );
 }
