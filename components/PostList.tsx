7cf70f715924887408189906fbbaf03ebc3b0d75
deve.hoge@gmail.com <61268214+dev-mori@users.noreply.github.com>
2020-11-26 21:20:34 +0900

    Home画面デザイン #52 修正版

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import PostListItem from './PostListItem';
 import { GET_ARTICLES } from '../client_hooks/articles';
-import Button from '@material-ui/core/Button';
-import { DELETE_ARTICLE } from '../client_hooks/articles';
 import { useMutation, useQuery } from '@apollo/client';
 import { makeStyles } from '@material-ui/core/styles';
+// import Button from '@material-ui/core/Button';
+// import { DELETE_ARTICLE } from '../client_hooks/articles';
 
 const useStyles = makeStyles(() => ({
   list: {
@@ -30,28 +30,28 @@ const PostList = () => {
       set_articles(data.articles);
     },
   });
-  const [delete_article] = useMutation(DELETE_ARTICLE);
-  const deletearticle = async (deleteid: number) => {
-    try {
-      await delete_article({
-        variables: {
-          id: deleteid,
-        },
-      });
-      set_articles(
-        articles?.filter((article: Article) => deleteid !== article.id)
-      );
-    } catch (er) {
-      console.log(er);
-    }
-  };
+  // const [delete_article] = useMutation(DELETE_ARTICLE);
+  // const deletearticle = async (deleteid: number) => {
+  //   try {
+  //     await delete_article({
+  //       variables: {
+  //         id: deleteid,
+  //       },
+  //     });
+  //     set_articles(
+  //       articles?.filter((article: Article) => deleteid !== article.id)
+  //     );
+  //   } catch (er) {
+  //     console.log(er);
+  //   }
+  // };
   return (
     <>
       {articles &&
         articles.map((item: Article) => (
           <li className={classes.list} key={item.id}>
             <PostListItem data={item} />
-            <Button onClick={() => deletearticle(item.id)}>delete</Button>
+            {/* <Button onClick={() => deletearticle(item.id)}>delete</Button> */}
           </li>
         ))}
     </>
