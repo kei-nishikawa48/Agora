fa39394507b39c162fe26b42c91a4c0690010e7c
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-03 23:04:15 +0900

    signupはhistory.back使わない

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -83,7 +83,7 @@ export default function SignUp() {
           password: data.pass,
         },
       });
-      history.back();
+      location.replace('/');
     } catch (er) {
       console.log(er);
     }
