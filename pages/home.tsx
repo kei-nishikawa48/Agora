0882a016578d6183524ebd3e822b32b4f7404af5
kei <xnkcelebrate48x@yahoo.co.jp>
2020-12-04 21:17:31 +0900

    なんとかSSGできた

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import PostList from '../components/PostList';
 import { makeStyles } from '@material-ui/core/styles';
-import Header from '../templates/Header';
+import Layout from '../components/Layout';
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
@@ -16,11 +16,12 @@ const useStyles = makeStyles((theme) => ({
 export default function Home() {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <Header />
-      <div className={classes.articles}>
-        <PostList />
+    <Layout>
+      <div className={classes.root}>
+        <div className={classes.articles}>
+          <PostList />
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
--- pages/posts/[id]/index.tsx
+++ pages/posts/[id]/index.tsx
@@ -1,20 +1,60 @@
 import Layout from '../../../components/Layout';
 import PostListDetail from '../../../components/PostListDetail';
-import { useRouter } from 'next/router';
-import { GET_ARTICLE } from '../../../client_hooks/articles';
-import { useQuery } from '@apollo/client';
+import { createApolloFetch } from 'apollo-fetch';
+import { GetStaticProps, GetStaticPaths } from 'next';
+import { Post } from '../../../interfaces';
 
+const fetch = createApolloFetch({
+  uri: 'http://localhost:3000/graphql',
+});
 
-const StaticPropsDetail = () => {
-  const router = useRouter();
-  const { data } = useQuery(GET_ARTICLE, {
-    variables: { id: router.query.id },
-  });
+const StaticPropsDetail = ({ data }: { data: { article: Post } }) => {
   return (
     <Layout title={`${data && data.article.title}`}>
       {data && <PostListDetail item={data.article} />}
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const postId = context.params?.id; // context.params で、paths で指定した params を参照できる
+  const query = `query get_article($id:ID!){
+  article(id:$id){
+    text
+    id
+    title
+  }
+}`;
+  const res = await fetch({
+    query: query,
+    variables: {
+      id: postId,
+    },
+  });
+  const data = res.data;
+
+  // 実際はここで await fetch(`https://.../posts/${postId}`) などとして外部 API からデータを取得するイメージ
+  return {
+    props: { postId, data },
+  };
+};
+
+export const getStaticPaths:GetStaticPaths = async () => {
+  const data = await fetch({
+    query: `{
+        articles {
+          id
+        }
+      }
+      `,
+  });
+  const paths = data?.data?.articles?.map((article: Post) => {
+    return `/posts/${article.id}`;
+  });
+  return {
+    paths: paths,
+    fallback: false,
+  };
 };
 
 export default StaticPropsDetail;
