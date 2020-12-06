ccd21a6ac4ef01f47f491ac5473680ba80295fa9
kei <xnkcelebrate48x@yahoo.co.jp>
2020-12-06 21:27:09 +0900

    修正

--- components/HeaderItem.tsx
+++ components/HeaderItem.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import Button from '@material-ui/core/Button';
 import { useCookies } from 'react-cookie';
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -5,6 +6,8 @@ import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import Router from 'next/router';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 const useStyles = makeStyles({
   button: {
     textTransform: 'none',
@@ -23,6 +26,15 @@ const useStyles = makeStyles({
 });
 
 const HeaderItem = () => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
   const classes = useStyles();
   const [cookies] = useCookies(['token']);
   return (
@@ -58,8 +70,20 @@ const HeaderItem = () => {
             aria-controls="menu-appbar"
             aria-haspopup="true"
             color="inherit"
+            onClick={handleClick}
           >
             <AccountCircle className={classes.icon} />
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
           </IconButton>
         </>
       )}
